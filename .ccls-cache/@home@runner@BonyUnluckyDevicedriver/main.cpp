dd36aadce870e3726fe1bac48a155850a1bb6037
Виктор Гоцык <102416942+HotsykVictor@users.noreply.github.com>
2022-03-31 05:36:22 +0000

    Create file

--- .ccls-cache/@home@runner@BonyUnluckyDevicedriver/main.cpp
+++ .ccls-cache/@home@runner@BonyUnluckyDevicedriver/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+using namespace std;
+int main()
+{
+ double z, k = 0;
+ int A[10], s=0;
+ cout << "Введіть 10 чисел" << endl;
+ for (int i=0; i<10; i++)
+  { cin >> A[i];
+   if(A[i]>0){
+     k++;
+    s+=A[i];
+    }
+  }
+  z=s/k;
+ cout << "Cереднє арифметичне додатних елементів "<< z << endl;
 
-int main() {
-  std::cout << "Hello World!\n";
+ return 0;
 }
